988d8f9098ece2798bafe2a06ec898ee989f07a1
D0lph1nnnnn <artjom.zur@gmail.com>
2020-01-21 11:12:20 +0200

    EZ

--- app.js
+++ app.js
@@ -4,4 +4,17 @@ github.getUserData()
 .then(data => {
     console.log(data.profile);
     console.log(data.repos);
+
+    function myFunction(imgs) {
+        // Get the expanded image
+        var expandImg = document.getElementById("expandedImg");
+        // Get the image text
+        var imgText = document.getElementById("imgtext");
+        // Use the same src in the expanded image as the image being clicked on from the grid
+        expandImg.src = imgs.src;
+        // Use the value of the alt attribute of the clickable image as text inside the expanded image
+        imgText.innerHTML = imgs.alt;
+        // Show the container element (hidden with CSS)
+        expandImg.parentElement.style.display = "block";
+    } 
 });
